a621ae222904ec692f4adc891654a1cf22c18539
bamcasa <combeesang@gmail.com>
2025-03-13 04:47:29 +0900

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
-export default defineConfig({
-  plugins: [svelte({ preprocess: vitePreprocess() })],
-  base: "/tetration/",
-});
+export default {
+  // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+};
